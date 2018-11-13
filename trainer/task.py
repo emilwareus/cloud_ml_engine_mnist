1db97b7b07ad2f93192aeb06ddaed4132543f8be
NikeNano <niklas.sven.hansson@gmail.com>
2018-11-13 17:06:24 +0100

    updated git ignore

--- trainer/task.py
+++ trainer/task.py
@@ -21,7 +21,7 @@ def main ():
         config=run_config
         )
     tf.logging.set_verbosity(tf.logging.INFO)
-    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=100)
+    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=200)
     exporter = tf.estimator.LatestExporter('exporter', serving_input_fn)
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn,
         start_delay_secs=30, 
@@ -31,6 +31,6 @@ def main ():
     tf.estimator.train_and_evaluate(mnist_classifier, train_spec, eval_spec)
     print("lets do something")
 
-    
+
 if __name__=="__main__":
     main()
